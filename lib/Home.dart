b468f980f5598fb3cccfca8b8c5d46d55e219d78
Harshan <sreeharshan920@gmail.com>
2022-09-25 22:39:51 +0530

    firstApp loginpage commit

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
+void main (){
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
 
 class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+    appBar:AppBar(
+    title: const Text("My Home List"),
+    ),
+      body: ListView(
+        children:const [
+        Text("Item1"),
+        Text("Item2"),
+        Text("Item3"),
+        Text("Item4"),
+        Text("Item5"),
+        ],
+      ),
+    );
   }
-
 }
